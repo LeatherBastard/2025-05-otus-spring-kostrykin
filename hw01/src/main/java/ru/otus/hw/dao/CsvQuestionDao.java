a0083c98efdb5087c6a2a094190a2957164d63d7
LeatherBastard <kostrykinmark@gmail.com>
2025-06-13 19:47:02 +0300

    fix: Transfer resource opening at getAll method and wrap it into try with resources block

--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,43 +1,43 @@
 package ru.otus.hw.dao;
 
 import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
 import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 
-
+@RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
-    private static final String FILE_NOT_FOUND_EXCEPTION_MESSAGE = "File not found";
+    private static final String FILE_NOT_FOUND_MESSAGE = "File not found!";
 
     private final TestFileNameProvider fileNameProvider;
 
-    private final InputStream inputStream;
-
-    public CsvQuestionDao(TestFileNameProvider fileNameProvider) {
-        this.fileNameProvider = fileNameProvider;
-        inputStream = fileNameProvider.getClass()
-                .getClassLoader()
-                .getResourceAsStream(this.fileNameProvider
-                        .getTestFileName());
-        if (inputStream == null) {
-            throw new QuestionReadException(FILE_NOT_FOUND_EXCEPTION_MESSAGE);
-        }
-
-    }
-
     @Override
     public List<Question> findAll() {
-        return new CsvToBeanBuilder<QuestionDto>(new InputStreamReader(inputStream))
-                .withSkipLines(1)
-                .withType(QuestionDto.class)
-                .withSeparator(';')
-                .build().parse()
-                .stream().map(QuestionDto::toDomainObject)
-                .toList();
+        try (InputStream inputStream = fileNameProvider.getClass()
+                .getClassLoader()
+                .getResourceAsStream(this.fileNameProvider
+                        .getTestFileName())) {
+            if (inputStream == null) {
+                throw new QuestionReadException(FILE_NOT_FOUND_MESSAGE);
+            }
+            return new CsvToBeanBuilder<QuestionDto>(new InputStreamReader(inputStream))
+                    .withSkipLines(1)
+                    .withType(QuestionDto.class)
+                    .withSeparator(';')
+                    .build().parse()
+                    .stream().map(QuestionDto::toDomainObject)
+                    .toList();
+        } catch (IOException e) {
+            throw new QuestionReadException(e.getMessage(), e);
+        }
+
+
     }
 }
