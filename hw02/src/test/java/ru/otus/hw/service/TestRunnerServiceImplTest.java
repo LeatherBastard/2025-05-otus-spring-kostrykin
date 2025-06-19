7bd7f383c350f85adec8777a55ae06cc471882f7
LeatherBastard <kostrykinmark@gmail.com>
2025-06-19 18:12:06 +0300

    feat: First working version

---
+++ hw02/src/test/java/ru/otus/hw/service/TestRunnerServiceImplTest.java
@@ -0,0 +1,33 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.inOrder;
+
+ class TestRunnerServiceImplTest {
+    private final TestService testService = Mockito.mock(TestService.class);
+
+    private final StudentService studentService = Mockito.mock(StudentService.class);
+
+    private final ResultService resultService = Mockito.mock(ResultService.class);
+
+    private TestRunnerService testRunnerService;
+
+    @BeforeEach
+    void initialize() {
+        testRunnerService = new TestRunnerServiceImpl(testService, studentService, resultService);
+    }
+
+    @Test
+    void whenRun_thenSuccessful() {
+        testRunnerService.run();
+        InOrder order = inOrder(studentService, testService, resultService);
+        order.verify(studentService).determineCurrentStudent();
+        order.verify(testService).executeTestFor(any());
+        order.verify(resultService).showResult(any());
+    }
+}
---
+++ hw02/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -0,0 +1,47 @@
+package ru.otus.hw.service;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.domain.Student;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+class TestServiceImplTest {
+
+    private final IOService ioService = Mockito.mock(IOService.class);
+
+
+    private final QuestionDao questionDao = Mockito.mock(QuestionDao.class);
+
+
+    private TestService testService;
+
+    @BeforeEach
+    void initialize() {
+        testService = new TestServiceImpl(ioService, questionDao);
+    }
+
+
+    @Test
+    void whenExecuteTest_thenSuccessful() {
+        Student student = new Student("Mark", "Kostrykin");
+        List<Question> questions = List.of(
+                new Question("What is the result of 2+2?",
+                        List.of(new Answer("4", true), new Answer("5", false))));
+        when(questionDao.findAll()).thenReturn(questions);
+        when(ioService.readIntForRangeWithPrompt(anyInt(), anyInt(), anyString(), anyString())).thenReturn(1);
+        testService.executeTestFor(student);
+        verify(ioService, atLeast(1)).printLine(anyString());
+        verify(ioService, atLeast(1)).printFormattedLine(anyString());
+        verify(ioService, atLeast(1)).readIntForRangeWithPrompt(anyInt(), anyInt(), anyString(), anyString());
+        verify(questionDao).findAll();
+    }
+
+}
