9e6b7fde98ed21671f11736975bf4a92f7375aa7
LeatherBastard <kostrykinmark@gmail.com>
2025-06-20 13:37:38 +0300

    fix: Correct wrong variables order

--- hw02/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -7,17 +7,13 @@ import org.springframework.stereotype.Component;
 @Component
 @Data
 public class AppProperties implements TestConfig, TestFileNameProvider {
+    private int rightAnswersCountToPass;
 
-    public AppProperties(@Value("${test.rightAnswersCountToPass}") int rightAnswersCountToPass, @Value("${test.fileName}") String testFileName) {
+    private String testFileName;
+
+    public AppProperties(@Value("${test.rightAnswersCountToPass}") int rightAnswersCountToPass,
+                         @Value("${test.fileName}") String testFileName) {
         this.rightAnswersCountToPass = rightAnswersCountToPass;
         this.testFileName = testFileName;
     }
-
-    // внедрить свойство из application.properties
-
-    private int rightAnswersCountToPass;
-
-    // внедрить свойство из application.properties
-
-    private String testFileName;
 }
