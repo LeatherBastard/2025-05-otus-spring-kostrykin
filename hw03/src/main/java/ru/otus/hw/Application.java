455fb2b05fcca9b389c1b42b00fadab8d84e2fdf
LeatherBastard <kostrykinmark@gmail.com>
2025-07-06 20:54:50 +0300

    fix: remove unused imports

--- hw03/src/main/java/ru/otus/hw/Application.java
+++ hw03/src/main/java/ru/otus/hw/Application.java
@@ -1,6 +1,5 @@
 package ru.otus.hw;
 
-import org.springframework.beans.factory.support.AbstractAutowireCapableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
