d6de2526c5423bfd78d1d10388443b08e0e0b0a0
LeatherBastard <kostrykinmark@gmail.com>
2025-08-18 20:13:10 +0300

    fix: Solve all suggestions

--- hw10/src/test/java/ru/otus/hw/controllers/AuthorControllerTest.java
+++ hw10/src/test/java/ru/otus/hw/controllers/AuthorControllerTest.java
@@ -1,18 +1,24 @@
 package ru.otus.hw.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.otus.hw.controllers.view.AuthorController;
+import ru.otus.hw.dto.author.AuthorDto;
 import ru.otus.hw.services.AuthorService;
 
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthorController.class)
 public class AuthorControllerTest {
+
 
     @Autowired
     private MockMvc mvc;
@@ -20,9 +26,17 @@ public class AuthorControllerTest {
     @MockBean
     private AuthorService authorService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    private List<AuthorDto> authors = List.of(new AuthorDto(1, "Author_1"),
+            new AuthorDto(2, "Author_2"));
+
     @Test
-    void shouldRenderAuthorsPage() throws Exception {
-        mvc.perform(get("/authors"))
-                .andExpect(view().name("authors"));
+    void shouldReturnAuthors() throws Exception {
+        when(authorService.findAll()).thenReturn(authors);
+        mvc.perform(get("/api/authors")).andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(authors)));
     }
 }
