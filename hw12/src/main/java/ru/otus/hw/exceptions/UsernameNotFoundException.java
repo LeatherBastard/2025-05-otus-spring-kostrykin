5087562b1b76805065abd552e3cf4367b16ae8aa
LeatherBastard <kostrykinmark@gmail.com>
2025-08-31 20:59:51 +0300

    fix: Remove redundant UsernameNotFoundException class

--- hw12/src/main/java/ru/otus/hw/exceptions/UsernameNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.hw.exceptions;
-
-public class UsernameNotFoundException extends RuntimeException {
-    public UsernameNotFoundException(String message) {
-        super(message);
-    }
-}
